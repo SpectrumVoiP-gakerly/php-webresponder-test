edacde430328a41f2f96797a1f21dbe1e2378aa3
SpectrumVoiP-gakerly <143219558+SpectrumVoiP-gakerly@users.noreply.github.com>
2025-08-04 16:13:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "<?xml version='1.0' encoding='UTF-8'?>\n";
 ?>
 <Response>
   <Say>Hello, this is a message before forwarding your call.</Say>
-  <Forward ByCaller='yes'>+19728240064</Forward>
 </Response>
+<Forward ByCaller='yes'>+19728240064</Forward>
