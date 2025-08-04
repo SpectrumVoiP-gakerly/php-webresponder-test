99e2f44904362599dbffd866ce017b032d868cfc
SpectrumVoiP-gakerly <143219558+SpectrumVoiP-gakerly@users.noreply.github.com>
2025-08-04 16:06:14 -0500

    Update index.php
    added real phone number

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "<?xml version='1.0' encoding='UTF-8'?>\n";
 ?>
 <Response>
   <Say>Hello, this is a message before forwarding your call.</Say>
-  <Forward>1234567890</Forward>
+  <Forward>14694292500</Forward>
 </Response>
