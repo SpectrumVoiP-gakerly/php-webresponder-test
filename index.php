cd545aaa2c26cd5d3280a88666eeca0cf661bbfb
SpectrumVoiP-gakerly <143219558+SpectrumVoiP-gakerly@users.noreply.github.com>
2025-08-06 14:10:11 -0500

    Update index.php
    8900 testing

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $caller = $_GET['NmsAni'] ?? '';
 // Define routing rules based on ANI
 $routes = [
     '2010' => 'sip:19728240064@*',
-    '+19728240064' => '2010',
+    '+19728240064' => '8900',
     '2030' => '2044',
     // Add more as needed
 ];
